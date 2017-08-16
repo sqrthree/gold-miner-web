a853ae0f00f2c6e461f586cf74622d5be366c41b
sqrtthree <imsqrtthree@gmail.com>
2017-08-16 19:07:29 +0800

    feat(views): Update the latest user information when assessing the page

--- FrontEnd/src/services/users.js
+++ FrontEnd/src/services/users.js
@@ -4,6 +4,10 @@ export const logout = function logout() {
   return axios.get('/auth/logout')
 }
 
+export const fetchUserInfo = function fetchUserInfo(id) {
+  return axios.get(`/api/users/${id}`)
+}
+
 export const validateInvitationCode = function validateInvitationCode(invitationCode) {
   return axios.post('/api/auth/validate-invitation-code', {
     invitationCode,
